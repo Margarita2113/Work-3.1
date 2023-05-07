ee8c0bec77e6d180497b73b49ce5a6108ee1b4e4
Oskina Margarita <rita_2113@mail.ru>
2023-05-07 11:29:21 +0300

    Works

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Объявлям переменную для одну для хранения стоимости билета
+        int ticketCost = 1450;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // Объявлям переменную для хранения количества рублей для одной бонусной мили
+        int milesPerRur = 20;
+
+        // Рассчитываем количество бонусных миль
+        int miles = (ticketCost / milesPerRur);
+
+        // Выводим результат на экран
+        System.out.println("Количество бонусных миль: " + miles);
     }
 }
